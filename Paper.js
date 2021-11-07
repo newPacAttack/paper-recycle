f13395db5c0cb339d6581c0e1f3a4fe624132cd9
newPacAttack <91351390+newPacAttack@users.noreply.github.com>
2021-11-07 12:44:47 -0700

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2,
+        }
+        this.body = Bodies.rectangle(x, y, 50, 50, options);
+        this.width = 50;
+        this.height = 50;
+        this.Image = loadImage("paper.png");
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        pos.x = mouseX;
+        pos.y = mouseY;
+        var angle  = this. body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this. width, this.width);
+        pop();
+    }
+
+}
